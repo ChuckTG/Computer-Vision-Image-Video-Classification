a52b8ea2ea2168525240e8f4cb5315a7bce85a72
ChuckTG <ktzirvelakis@gmail.com>
2020-07-17 19:15:44 +0300

    CNN architecture

--- small_cnn.py
+++ small_cnn.py
@@ -23,39 +23,72 @@ train_ollie_dir = os.path.join(train_dir,'ollie')
 # Just normalization for validation
 image_transforms = {
     'train': transforms.Compose([
-        transforms.RandomResizedCrop(150),
+        transforms.Resize(155),
+        transforms.CenterCrop(150),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
     'val': transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
+        transforms.Resize(150),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
+    ])
 }
 
-data_dir = 'data/2dconv'
-image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
-                                          image_transforms[x])
-                  for x in ['train', 'val']}
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
-                                             shuffle=True, num_workers=4)
-              for x in ['train', 'val']}
-dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
-class_names = image_datasets['train'].classes
+
+
+
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # load the dataset
-skate_dataset = datasets.ImageFolder(root = train_dir,
-                                     transform = image_transforms['train'])
+data_dir = 'data/2dconv'
 
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                          image_transforms[x])
+                  for x in ['train', 'val']}
+
+class_names = image_datasets['train'].classes
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=6,
+                                             shuffle=True, num_workers=4)
+              for x in ['train', 'val']}
 
 #map indexes to classes
-idx_to_class = {v:k for k,v in skate_dataset.class_to_idx.items()}
+idx_to_class = {v:k for k,v in image_datasets['train'].class_to_idx.items()}
 
-print(skate_dataset,"\n\nClasses of the dataset",skate_dataset.classes)
+print(image_datasets['train'],"\n\nClasses of the dataset",image_datasets['train'].classes)
 print('index to class:',idx_to_class)
 
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.block1 = self.convolution(c_in=3, c_out=16,kernel_size = 3, stride = 1)
+        self.block2 = self.convolution(c_in=16,c_out=32,kernel_size=3,stride = 1)
+        self.block3 = self.convolution(c_in = 32,c_out =64, kernel_size=3,stride=1)
+
+    def forward(self,x):
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = x.view(-1,64*17*17)
+        return self.linear_layers(x)
+
+    def convolution(self,c_in,c_out, **kwargs):
+        seq_block = nn.Sequential(
+            nn.Conv2d(in_channels= c_in,out_channels=c_out,**kwargs),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        return seq_block
+
+    def linear_layers(self):
+        self.linear = nn.Sequential(
+            nn.Linear(in_features = 17*17*64,out_features=512),
+            nn.ReLU(),
+            nn.Linear(512,1),
+            nn.Sigmoid()
+        )
